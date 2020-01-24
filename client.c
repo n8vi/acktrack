defe4238f5be81c9ad6b34795e6b0a45cfdeb6d0
Brad Tarratt <bradn8vi@gmail.com>
2020-01-24 03:08:11 -0500

    mostly functional demo

--- client.c
+++ client.c
@@ -9,6 +9,7 @@
 #include <errno.h>
 #include <arpa/inet.h>
 #include <pcap.h>
+#include <sys/select.h>
 
 void error(char *msg)
 {
@@ -16,7 +17,7 @@ void error(char *msg)
     exit(0);
 }
 
-void callback(u_char *useless,const struct pcap_pkthdr* pkthdr,const u_char*
+void capsck_callback(u_char *useless,const struct pcap_pkthdr* pkthdr,const u_char*
         packet)
 {
   static int count = 1;
@@ -25,13 +26,12 @@ void callback(u_char *useless,const struct pcap_pkthdr* pkthdr,const u_char*
 }
 
 
-int getsocketinfo(int sck)
+pcap_t *capsck_create(int sck, char* errbuf)
 {
     struct sockaddr_in laddr;
     struct sockaddr_in raddr;
     socklen_t len;
     int ret;
-    char errbuf[PCAP_ERRBUF_SIZE];
     pcap_t *descr;
     struct pcap_pkthdr hdr;
     const u_char *packet;
@@ -42,26 +42,40 @@ int getsocketinfo(int sck)
     bpf_u_int32 pNet;
     bpf_u_int32 pMask;
 
-    printf("socket id %d\n", sck);
-
     len = sizeof(raddr);
 
     ret = getpeername(sck, (struct sockaddr*)&raddr, &len);
 
-    if (ret == -1)
-        return -1;
+    if (ret == -1) {
+        if (errno == ENOTCONN)
+            strcpy(errbuf, "Socket isn't connected\n");
+        else if (errno == EBADF)
+            strcpy(errbuf, "Bad socket descriptor\n");
+        else
+            strcpy(errbuf, "Unknown error\n");
+
+        return NULL;
+        }
 
     ret = getsockname(sck, (struct sockaddr*)&laddr, &len);
 
-    if (ret == -1)
-        return -1;
+    if (ret == -1) {
+        if (errno == ENOTCONN)
+            strcpy(errbuf, "Socket isn't connected\n");
+        else if (errno == EBADF)
+            strcpy(errbuf, "Bad socket descriptor\n");
+        else
+            strcpy(errbuf, "Unknown error\n");
+
+        return NULL;
+        }
 
     descr = pcap_open_live("any", BUFSIZ, 0, -1,errbuf);
 
     if(descr == NULL)
     {
-        printf("pcap_open_live(): %s\n",errbuf);
-        exit(1);
+        strcpy(errbuf, "pcap_compile failed");
+        return NULL;
     }
 
     sprintf((char*)source, "src host %s and src port %d", 
@@ -74,48 +88,39 @@ int getsocketinfo(int sck)
         ntohs(laddr.sin_port)
         );
 
+    /* inet_ntoa returns a static buffer so we can't just do this all at once */
     sprintf((char *)filter, "%s and %s", source, dest);
-    /* yes, there's a good reason to do this in two parts */
-
-    printf("%s\n", filter);
 
     pcap_lookupnet("any", &pNet, &pMask, errbuf);
 
     if(pcap_compile(descr, &fp, filter, 0, pNet) == -1) {
-        printf("\npcap_compile failed\n");
-        exit(1);
+        strcpy(errbuf, "pcap_compile failed");
+        return NULL;
         }
 
     if(pcap_setfilter(descr, &fp) == -1){
-        printf("\npcap_setfilter() failed\n");
-        exit(1);
+        strcpy(errbuf, "pcap_setfilter failed");
+        return NULL;
         }
 
-    pcap_loop(descr, -1, callback, NULL);
-
-    return 0;
+    return descr;
 }
 
-void showsocketinfo(int sck)
+void capsck_dispatch(pcap_t *descr)
 {
-    int ret;
-
-    ret = getsocketinfo(sck);
-    if (ret == -1)
-        if (errno == ENOTCONN)
-            printf("Socket isn't connected\n");
-        else if (errno == EBADF)
-            printf("Bad socket descriptor\n");
-        else
-            printf("Unknown error\n");
+    pcap_dispatch(descr, -1, capsck_callback, NULL);
 }
 
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n, ret;
 
+    char errbuf[PCAP_ERRBUF_SIZE];
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    pcap_t *capsck;
+    struct timeval t;
+    int i;
 
 
     char buffer[256];
@@ -142,16 +147,27 @@ int main(int argc, char *argv[])
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
 
+    capsck = capsck_create(sockfd, errbuf);
 
-    printf("\npostconnect: ");
-    showsocketinfo(sockfd);
+    if (capsck == NULL) {
+        printf("%s\n", errbuf);
+        }
 
-    close(sockfd);
+    // Perhaps in a thread?
+    while (1) {
+        capsck_dispatch(capsck);
+        t.tv_sec = 0;
+        t.tv_usec = 500;
+        select(0,NULL,NULL,NULL, &t);
+        i++;
+        i %= 100;
+        if (i == 0) {
+            printf("normal thing-doing loop here\n");
+            }
+        }
+        
+	
 
-    printf("\npostclose: ");
-    showsocketinfo(sockfd);
-
-    exit(0);
 
     printf("Please enter the message: ");
     bzero(buffer,256);
