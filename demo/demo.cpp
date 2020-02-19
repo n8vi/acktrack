3bf31223cea389a3f47ce655855a14faf98c0db1
Brad Tarratt <brad.tarratt@p3-group.com>
2020-02-19 10:12:24 -0500

    Cleaned up visual studio files

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,24 +1,28 @@
 #include "pcapsocket.h"
 #include <stdio.h>
 
-void error(char *msg)
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
+
+void error(char* msg)
 {
     perror(msg);
     exit(0);
 }
 
 
-void mycallback(capsck_t *cs, sequence_event_t *se)
+void mycallback(capsck_t* cs, sequence_event_t* se)
 {
-  if (se->is_local) {
-    printf("  <-- SEQ %lu.%lu: %d\n", se->ts.tv_sec, se->ts.tv_usec, se->seqno);
-  } else {
-    printf("  --> ACK %lu.%lu: %d\n", se->ts.tv_sec, se->ts.tv_usec, se->seqno);
+    if (se->is_local) {
+        printf("  <-- SEQ %lu.%lu: %d\n", se->ts.tv_sec, se->ts.tv_usec, se->seqno);
+    }
+    else {
+        printf("  --> ACK %lu.%lu: %d\n", se->ts.tv_sec, se->ts.tv_usec, se->seqno);
     }
 }
 
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 
 #ifndef WIN32
@@ -26,17 +30,17 @@ int main(int argc, char *argv[])
 #else
     SOCKET sockfd;
 #endif
-    
+
     int portno, n; // , ret;
 
     char errbuf[PCAP_ERRBUF_SIZE];
     struct sockaddr_in serv_addr;
-    struct hostent *server;
-    capsck_t *capsck;
+    struct hostent* server;
+    capsck_t* capsck;
     // struct timeval t;
     int i = 0;
     char buffer[256];
- 
+
     int iResult;
 
 #ifdef WIN32
@@ -50,31 +54,31 @@ int main(int argc, char *argv[])
 #endif
 
     printf("\n\n");
- 
+
     if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+        exit(0);
     }
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+    if (sockfd < 0)
+        error((char*)"ERROR opening socket");
 
     printf("Connecting to host %s port %d\n", argv[1], portno);
     server = gethostbyname(argv[1]);
     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
+        fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    bzero((char*)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char*)server->h_addr,
+        (char*)&serv_addr.sin_addr.s_addr,
+        server->h_length);
     serv_addr.sin_port = htons(portno);
 
-    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
+    if (connect(sockfd, (struct sockaddr*) & serv_addr, sizeof(serv_addr)) < 0)
+        error((char*)"ERROR connecting");
 
     capsck = capsck_create(sockfd, errbuf, mycallback);
 
@@ -84,20 +88,20 @@ int main(int argc, char *argv[])
     if (capsck == NULL) {
         fprintf(stderr, "%s\n", errbuf);
         exit(0);
-        }
+    }
 
     strcpy(buffer, "GET /\r\n");
-    n = send(sockfd, buffer, strlen(buffer),0);
+    n = send(sockfd, buffer, strlen(buffer), 0);
     if (n < 0)
-        error("ERROR writing to socket");
+        error((char*)"ERROR writing to socket");
 
 
     // Perhaps in a thread?
     while (1) {
         // n = read(sockfd, buffer, 255);
-        n = recv(sockfd, buffer, sizeof(buffer)-1, 0);
+        n = recv(sockfd, buffer, sizeof(buffer) - 1, 0);
         if (n < 0)
-            error("ERROR reading from socket");
+            error((char*)"ERROR reading from socket");
         if (n > 0) {
             capsck_dispatch(capsck);
             // printf("read %d octets\n", n);
@@ -117,6 +121,6 @@ int main(int argc, char *argv[])
         i++;
         i %= 100;
         // if (i == 0) printf("normal thing-doing loop here (last read %d)\n", n);
-        }
-        
+    }
+
 }
