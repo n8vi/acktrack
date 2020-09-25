8bcc9175067051aa9683f0df882d490a40b08f3a
Brad Tarratt <bradn8vi@gmail.com>
2020-09-25 18:27:18 -0400

    still experimenting with this method of testing

--- tests/fixture.cpp
+++ tests/fixture.cpp
@@ -1,3 +1,37 @@
+#include "../acktrack.h"
+
+acktrack_t *acktrack_alloc(void)
+{
+    acktrack_t *ret;
+    ret = (acktrack_t *)malloc(sizeof(acktrack_t));
+    printf("%p\n", ret);
+    return ret;
+}
+
+acktrack_t *acktrack_setaddrs(acktrack_t *acktrack, sockaddr *local, sockaddr *remote)
+{
+    memcpy(&acktrack->local, local, sizeof(acktrack->local));
+    memcpy(&acktrack->remote, remote, sizeof(acktrack->remote));
+    return acktrack;
+}
+
+acktrack_t *set_lseqorig(acktrack_t* acktrack, u_int new_lseqorig)
+{
+    acktrack->lseqorig = new_lseqorig;
+    return acktrack;
+}
+
+acktrack_t *set_rseqorig(acktrack_t* acktrack, u_int new_rseqorig)
+{
+    acktrack->rseqorig = new_rseqorig;
+    return acktrack;
+}
+
+void printaddr(void *p)
+{
+    printf("%p\n", p);
+}
+
 int main(void)
 {
 }
